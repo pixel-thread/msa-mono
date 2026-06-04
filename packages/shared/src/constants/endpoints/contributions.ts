d3eecd135b2b2659f960af5d040dcc31de91a628
JyrwaJR <jyrwaboys@gmail.com>
2026-06-04 19:42:28 +0530

    update endpoints

--- packages/shared/src/constants/endpoints/contributions.ts
+++ packages/shared/src/constants/endpoints/contributions.ts
@@ -1,15 +1,17 @@
 export const CONTRIBUTION = {
-  DECLARATIONS: '/contributions/declarations',
-  ALL_DECLARATIONS: '/contributions/all-declarations',
-  APPROVE_DECLARATION: (id: string) => `/contributions/declarations/${id}/approve`,
-  REJECT_DECLARATION: (id: string) => `/contributions/declarations/${id}/reject`,
+  DECLARATIONS: "/contributions/declarations",
+  ALL_DECLARATIONS: "/contributions/all-declarations",
+  APPROVE_DECLARATION: (id: string) =>
+    `/contributions/declarations/${id}/approve`,
+  REJECT_DECLARATION: (id: string) =>
+    `/contributions/declarations/${id}/reject`,
 
-  LIST: '/contributions/contributions',
-  GENERATE: '/contributions/contributions',
-  WAIVE: '/contributions/contributions',
+  LIST: "/contributions/contributions",
+  GENERATE: "/contributions/contributions",
+  WAIVE: "/contributions/contributions",
   DETAIL: (id: string) => `/contributions/contributions/${id}`,
 
   USER: (userId: string) => `/contributions/users/${userId}`,
 
-  CREATE_PAYMENT: '/contributions/payments',
+  CREATE_PAYMENT: "/contributions/record",
 } as const;
