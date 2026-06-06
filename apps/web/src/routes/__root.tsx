e2d3daa219e74b0b072f991f3388ad0103f05a8f
JyrwaJR <jyrwaboys@gmail.com>
2026-06-06 22:24:48 +0530

    fix font

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,6 +1,7 @@
-import '@fontsource/roboto';
-import '@fontsource/roboto-mono';
-import '@fontsource/jetbrains-mono';
+import '@fontsource/jetbrains-mono/400.css';
+import '@fontsource/jetbrains-mono/500.css';
+import '@fontsource/jetbrains-mono/600.css';
+import '@fontsource/jetbrains-mono/700.css';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { AppProviders } from '@src/shared/providers/AppProviders';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
