ab88832dd6ed733cc10c0236690b3b90ac5bc103
JyrwaJR <jyrwaboys@gmail.com>
2026-06-07 11:56:23 +0530

    update

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,3 +1,4 @@
+import pluginQuery from '@tanstack/eslint-plugin-query';
 import tanstackRouter from '@tanstack/eslint-plugin-router';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import reactHooks from 'eslint-plugin-react-hooks';
@@ -5,6 +6,7 @@ import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import tseslint from 'typescript-eslint';
 
 const eslintConfig = defineConfig([
+  ...pluginQuery.configs['flat/recommended'],
   ...tanstackRouter.configs['flat/recommended'],
   ...tseslint.configs.recommended,
   {
