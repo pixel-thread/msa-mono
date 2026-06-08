efad794992c260b0c213cd3bb3997a9515fa10fe
JyrwaJR <jyrwaboys@gmail.com>
2026-06-08 10:18:11 +0530

    fix csrf bug

--- apps/backend/src/middleware/csrf.ts
+++ apps/backend/src/middleware/csrf.ts
@@ -4,7 +4,15 @@ import { env } from '@src/env';
 import { API_PUBLIC_ROUTES } from '@src/shared/constants';
 import { logger } from '@src/shared/logger';
 import { getTraceId } from '@utils';
-import type { NextFunction, Request, Response } from 'express';
+import type { CookieOptions, NextFunction, Request, Response } from 'express';
+
+const csrfCookieOptions: CookieOptions = {
+  httpOnly: false,
+  secure: env.NODE_ENV === 'production',
+  sameSite: env.NODE_ENV === 'production' ? 'none' : 'lax',
+  path: '/',
+  maxAge: 60 * 60 * 24,
+};
 
 export function csrf(req: Request, res: Response, next: NextFunction) {
   const traceId = getTraceId();
@@ -21,27 +29,25 @@ export function csrf(req: Request, res: Response, next: NextFunction) {
   if (['GET', 'HEAD', 'OPTIONS'].includes(method)) {
     if (!req.cookies?.['csrf-token']) {
       const token = generateCsrfToken();
-      res.cookie('csrf-token', token, {
-        httpOnly: false,
-        secure: env.NODE_ENV === 'production',
-        sameSite: env.NODE_ENV === 'production' ? 'none' : 'lax',
-        path: '/',
-        maxAge: 60 * 60 * 24,
-      });
+      res.cookie('csrf-token', token, csrfCookieOptions);
     }
     return next();
   }
 
   const csrfCookie = req.cookies?.['csrf-token'];
 
-  const csrfHeader = req.headers['x-csrf-token'] as string | undefined;
+  const csrfHeader = req.headers['x-csrf-token'];
 
   if (!csrfCookie && !csrfHeader) {
     logger.info({ csrfCookie, csrfHeader, traceId }, 'CSRF cookie and header are not present');
+    const token = generateCsrfToken();
+    res.cookie('csrf-token', token, csrfCookieOptions);
     return next(new ForbiddenError('Invalid CSRF token'));
   }
 
   if (!verifyCsrfToken(csrfHeader as string, csrfCookie)) {
+    const token = generateCsrfToken();
+    res.cookie('csrf-token', token, csrfCookieOptions);
     return next(new ForbiddenError('Invalid or missing CSRF token'));
   }
 
