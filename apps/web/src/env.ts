8ea091d48be9da6cd2870eeeceae8fb6e89e0a17
JyrwaJR <jyrwaboys@gmail.com>
2026-06-09 20:59:57 +0530

    u[date env to verify what is not working

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -1,10 +1,17 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  NEXT_PUBLIC_APP_URL: z.url(),
-  NEXT_PUBLIC_ASSOCIATION_SLUG: z.string().min(2).max(10).default('mfsa'),
-  NEXT_PUBLIC_API_BASE_URL: z.url(),
-  NEXT_PUBLIC_NODE_ENV: z.enum(['development', 'test', 'production']),
+  NEXT_PUBLIC_APP_URL: z.url('Invalid NEXT_PUBLIC_APP_URL'),
+  NEXT_PUBLIC_ASSOCIATION_SLUG: z
+    .string('INVALID NEXT_PUBLIC_ASSOCIATION_SLUG')
+    .min(2)
+    .max(10)
+    .default('mfsa'),
+  NEXT_PUBLIC_API_BASE_URL: z.url('Invalid NEXT_PUBLIC_API_BASE_URL'),
+  NEXT_PUBLIC_NODE_ENV: z.enum(
+    ['development', 'test', 'production'],
+    'Invalid NEXT_PUBLIC_NODE_ENV',
+  ),
 });
 
 function createEnv() {
@@ -12,11 +19,11 @@ function createEnv() {
     NEXT_PUBLIC_APP_URL: import.meta.env.NEXT_PUBLIC_APP_URL,
     NEXT_PUBLIC_ASSOCIATION_SLUG: import.meta.env.NEXT_PUBLIC_ASSOCIATION_SLUG,
     NEXT_PUBLIC_API_BASE_URL: import.meta.env.NEXT_PUBLIC_API_BASE_URL,
-    NEXT_PUBLIC_NODE_ENV: import.meta.env.NEXT_PUBLIC_NODE_ENV,
+    NEXT_PUBLIC_NODE_ENV: process.env.NODE_ENV,
   });
 
   if (!parsed.success) {
-    throw new Error('Invalid environment variables');
+    throw new Error('Invalid environment variables', parsed.error);
   }
 
   return parsed.data;
