b32628af0003b80e6b4b848f31d5d02c4876028a
JyrwaJR <jyrwaboys@gmail.com>
2026-07-14 15:12:49 +0530

    feat: support search and variadic args in meetings query keys

--- packages/shared/src/constants/query-keys/meetings.ts
+++ packages/shared/src/constants/query-keys/meetings.ts
@@ -1,12 +1,13 @@
+type Args = string | number | boolean | null | undefined;
 export const MEETINGS_KEYS = {
-  ALL:       () => ['meetings'].filter(Boolean),
-  LISTS:     () => ['meetings'].filter(Boolean),
-  LIST:      (page?: number) => ['meetings', page].filter(Boolean),
-  DETAIL:    (id: string) => ['meetings', id].filter(Boolean),
+  ALL: () => ['meetings'].filter(Boolean),
+  LISTS: () => ['meetings'].filter(Boolean),
+  LIST: (...args: Args[]) => ['meetings', ...args].filter(Boolean),
+  DETAIL: (id: string) => ['meetings', id].filter(Boolean),
   ATTENDEES: (meetingId: string) => ['meetings', meetingId, 'attendees'].filter(Boolean),
-  AGENDAS:   (meetingId: string) => ['meetings', meetingId, 'agendas'].filter(Boolean),
-  AGENDA:    (meetingId: string) => ['meetings', meetingId, 'agenda'].filter(Boolean),
-  MINUTES:   (meetingId: string) => ['meetings', meetingId, 'minutes'].filter(Boolean),
-  RSVPS:     (meetingId: string) => ['meetings', meetingId, 'rsvps'].filter(Boolean),
-  RSVP:      (meetingId: string) => ['meetings', meetingId, 'rsvp'].filter(Boolean),
-}
+  AGENDAS: (meetingId: string) => ['meetings', meetingId, 'agendas'].filter(Boolean),
+  AGENDA: (meetingId: string) => ['meetings', meetingId, 'agenda'].filter(Boolean),
+  MINUTES: (meetingId: string) => ['meetings', meetingId, 'minutes'].filter(Boolean),
+  RSVPS: (meetingId: string) => ['meetings', meetingId, 'rsvps'].filter(Boolean),
+  RSVP: (meetingId: string) => ['meetings', meetingId, 'rsvp'].filter(Boolean),
+};
